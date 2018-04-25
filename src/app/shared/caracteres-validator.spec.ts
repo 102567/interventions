6bcb66a958e108b75121cb3c41802ad4590ce5a8
Philippe Viau <35924394+102567@users.noreply.github.com>
2018-04-25 12:53:52 -0400

    validator et test sur la longeur

--- src/app/shared/caracteres-validator.spec.ts
+++ src/app/shared/caracteres-validator.spec.ts
@@ -1,4 +1,4 @@
-import { nombreCaractereValidator } from "./caracteres-validator";
+import { nombreCaractereValidator} from "./caracteres-validator";
 import { AbstractControl } from "@angular/forms";
 
 describe('sanEspace Validator', () => {
@@ -28,5 +28,37 @@ describe('sanEspace Validator', () => {
         let validator = nombreCaractereValidator.sansEspace();
         let result = validator(control as AbstractControl);
         expect(result['sansEspace']).toBe(true);
+    });
+
+    
+});
+
+describe('longueurMinimum', () =>{
+    it('Invalide avec 1 espace et 2 caractères', () => {
+        let control = { value: ' yy' };
+        let validator = nombreCaractereValidator.longueurMinimum(3);
+        let result = validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(false);
+    });
+
+    it('Invalide avec 2 espace et 1 caractères', () => {
+        let control = { value: '  y' };
+        let validator = nombreCaractereValidator.longueurMinimum(3);
+        let result = validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(false);
+    });
+
+    it('Valide avec 3 espace et 3 caractères', () => {
+        let control = { value: '   J’aime Angular' };
+        let validator = nombreCaractereValidator.longueurMinimum(3);
+        let result = validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(true);
+    });
+
+    it('Valide avec 5 espace et 5 caractères', () => {
+        let control = { value: '     J’aime Angular     ' };
+        let validator = nombreCaractereValidator.longueurMinimum(3);
+        let result = validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(true);
     });
 });
--- src/app/shared/caracteres-validator.ts
+++ src/app/shared/caracteres-validator.ts
@@ -9,4 +9,13 @@ export class nombreCaractereValidator{
             return { 'sansEspace': false};
         };
     }
+
+    static longueurMinimum(min: number): ValidatorFn {
+        return (c: AbstractControl): { [key: string]: boolean } | null => {
+            if (c.value.trim().length >= min){
+                return { 'longueurMinimum': true};
+            }
+            return { 'longueurMinimum': false};
+        };
+    }
 }
