1c1614f2876a1c01f8edd079786332dc6937bdc4
Philippe Viau <35924394+102567@users.noreply.github.com>
2018-05-09 16:49:15 -0400

    tp18

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -11,4 +11,29 @@ describe('interventions App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to inter!');
   });
+
+  it('doit activer le bouton sauvegarder avec champs valides scénario nominal', () => {
+    page.setChampsValidesScenarioNominal;
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('doit activer le bouton sauvegarder avec champs valides scénario alternatif Par message TEXTE', () => {
+    page.setChampsValidesScenarioAlternatifParMessageTexte;
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('doit activer le bouton sauvegarder avec champs valides scénario alternatif Par courriel', () => {
+    page.setChampsValidesScenarioAlternatifParCourriel;
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('zone descriptionProblème a une bordure rouge si le nombre de caractère est insuffisant', () => {
+    page.setZoneDescriptionProblemeCaractereInsuffisant;
+    expect(page.obtenirClasseZoneDescriptionProbleme()).toContain('is-invalid');
+  });
+
+  it('zone descriptionProblème a une bordure rouge si le nombre de caractère est insuffisant', () => {
+    page.setZoneDescriptionProblemeCaractereSuffisant;
+    expect(page.obtenirClasseZoneDescriptionProbleme()).toContain('is-valid');
+  });
 });
